4f091fcd757f73f66df4bd40602b1fabe43c6012
AIT AKKACHE Soufiane <saitakkache@agb.dz>
2016-11-09 09:57:49 +0100

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soufianeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the soufianeApp
+ */
+angular.module('soufianeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soufianeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the soufianeApp
+ */
+angular.module('soufianeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
